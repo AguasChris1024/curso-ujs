7ad1d1de073be04d84fbc8719e767e3424f1ac1c
AguasChris1024 <aguaschris35@gmail.com>
2023-02-19 00:21:04 -0500

    Lección 7 terminada

--- Leccion07-FuncionesJS/07-01-FuncionesJS-UJS.js
+++ Leccion07-FuncionesJS/07-01-FuncionesJS-UJS.js
@@ -3,6 +3,7 @@
 
 //Declaración de la función
 function miFuncion(a, b){
+    console.log(arguments.length);
     return a + b;
 }
 
@@ -12,9 +13,14 @@ console.log(resultado);
 
 //Funciones tipo expresión
 //Declaración de la función
-let sumar = function(a,b){return a+b};
+let sumar = function(a = 4, b = 5){
+    console.log(arguments[0]);
+    console.log(arguments[1]);
+    console.log(arguments[2]);
+    return a + b + arguments[2];
+};
 
-resultado = sumar(1,2);
+resultado = sumar(3, 6, 7);
 console.log(resultado);
 
 //Funciones Self-Invoking
@@ -23,3 +29,64 @@ console.log(resultado);
 (function(a, b){
     console.log('Ejecutando la función:' + (a + b));
 })(3,4);
+
+//Funciones como objetos
+console.log(typeof(miFuncion));
+
+var miFuncionTexto = miFuncion.toString();
+console.log(miFuncionTexto);
+
+
+//Funciones flecha
+// *Comparar con función de tipo expresión*
+const sumarFuncionTipoFlecha = (a, b) => a + b;
+resultado = sumarFuncionTipoFlecha(3, 5);
+console.log(resultado);
+
+// 58. Argumentos y parámetros
+/**
+ * Parámetros -> Valores que va a recibir la función 
+ * Argumentos -> Valores que asignamos a los parámetros de la función 
+*/
+
+/**59. Sumar todos los argumentos
+ * Hoisting
+ */
+let resultado1 = sumarTodo(5, 4, 13, 10, 9, 10, 11, 3);
+console.log(resultado1);
+ 
+function sumarTodo(){
+    let suma =  0;
+    for(let i = 0; i < arguments.length; i++){
+        suma += arguments[i]; //suma = suma + arguments[i]
+    }
+    return suma;
+}
+
+/**60 y 61. Paso por valor y paso por referencia
+ * Paso por valor -> Tipos de datos que no son objetos (Cuando se manda argumentos a las funciones)
+ */
+
+let x = 10; //Tipo primitivo
+
+function cambiarValor(a){
+    a = 20;
+}
+
+//Paso por valor
+cambiarValor(x);
+console.log(x);
+//console.log(a);
+
+const persona = {
+    nombre: 'Christian',
+    apellido: 'Aguas'
+}
+
+function cambiarValorObjeto(p1){
+    p1.nombre = 'Xavier';
+    p1.apellido = 'Núñez';
+}
+
+cambiarValorObjeto(persona);
+console.log(persona);
